50c9ad6eedec99b81c589e88ec5f48a484f9f87a
Phil Carns <carns@mcs.anl.gov>
2024-06-18 20:21:07 -0400

    new setup
    - volume control
    - track control
    - background control
    - fn
    - fn (on air)

--- keyboards/minimacro5/keymaps/carns/keymap.c
+++ keyboards/minimacro5/keymaps/carns/keymap.c
@@ -1,5 +1,9 @@
 #include QMK_KEYBOARD_H
 
+#undef ENCODER_RESOLUTION
+#define ENCODER_RESOLUTION 4
+#define NUMBER_OF_ENCODERS 3
+
 enum layers {
      _MAIN,
 };
@@ -7,15 +11,16 @@ enum layers {
 bool encoder_update_user(uint8_t index, bool clockwise) {
   if (index == 0) { /* First encoder*/
     if (clockwise) {
-      tap_code16(LCTL(KC_WH_U));
+      tap_code(KC_AUDIO_VOL_DOWN);
     } else {
-      tap_code16(LCTL(KC_WH_D));
+      tap_code(KC_AUDIO_VOL_UP);
     }
-  } else if (index == 1) { /* Second encoder*/
+  }
+  else if (index == 1) { /* Second encoder*/
     if (clockwise) {
-      tap_code(KC_WH_D);
+      tap_code(KC_MEDIA_PREV_TRACK);
     } else {
-      tap_code(KC_WH_U);
+      tap_code(KC_MEDIA_NEXT_TRACK);
     }
   } else if (index == 2) { /* Third encoder*/
     if (clockwise) {
@@ -23,25 +28,17 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else {
       tap_code(KC_F8);
     }
-  } else if (index == 3) { /* Fourth encoder*/
-    if (clockwise) {
-      tap_code(KC_7);
-    } else {
-      tap_code(KC_8);
-    }
-  } else if (index == 4) { /* Fifth encoder*/
-    if (clockwise) {
-      tap_code(KC_9);
-    } else {
-      tap_code(KC_0);
-    }
   }
-  return true;
+  return false;
 }
 
 //
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
   [_MAIN] = LAYOUT_ortho_1x5(
-     LCTL(KC_0), LCTL(KC_HOME), KC_F7, KC_F9, KC_F10
+#if 1
+     KC_AUDIO_MUTE, KC_MEDIA_PLAY_PAUSE, KC_F7, KC_F9, KC_F10
+#else
+     KC_AUDIO_MUTE, LCTL(KC_HOME), KC_F7, KC_F9, KC_F10
+#endif
   )
 };
